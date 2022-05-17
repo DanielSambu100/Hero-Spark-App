39d5a55e549e681713de6598e54735c288da26bb
DanielSambu100 <daniel.wanyonyi@stdent.moringaschool.com>
2022-05-17 10:21:48 +0300

    update app

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,9 @@
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static spark.Spark.*;
@@ -19,9 +21,29 @@ public class App {
 
         port(getHerokuAssignedPort());
         staticFileLocation("/public");
-        get("/", (request, response) -> { //request for route happens at this location
+        get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>(); // new model is made to store information
             return new ModelAndView(model, "index.hbs"); // assemble individual pieces and render
         }, new HandlebarsTemplateEngine()); //
+
+        get("/hero",(request, response) -> {
+            List<Object> list=new ArrayList<>();
+            Map<String, Object> model = new HashMap<String, Object>(); // new model is made to store information
+            list.add("hero1");
+            list.add("hero2");
+            list.add("hero3");
+            list.add("hero4");
+            return new ModelAndView(model, "hero.hbs"); // assemble individual pieces and render
+        }, new HandlebarsTemplateEngine()); //
+
+        get("/squad",(request, response) -> {
+            List<Object> list=new ArrayList<>();
+            Map<String, Object> model = new HashMap<String, Object>(); // new model is made to store information
+            list.add("squad1");
+            list.add("squad2");
+            list.add("squad3");
+            list.add("squad4");
+            return new ModelAndView(model, "squad.hbs"); // assemble individual pieces and render
+        }, new HandlebarsTemplateEngine()); //
+        }
     }
-}
