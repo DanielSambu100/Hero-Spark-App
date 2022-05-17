0e1eeead0619b1f4d4b23e6934ac1722160019bd
DanielSambu100 <daniel.wanyonyi@stdent.moringaschool.com>
2022-05-17 09:16:43 +0300

    update app

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,27 @@
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
+
 public class App {
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+
     public static void main(String[] args) {
-        get("/", (request,response))
+
+        port(getHerokuAssignedPort());
+        staticFileLocation("/public");
+        get("/", (request, response) -> { //request for route happens at this location
+            Map<String, Object> model = new HashMap<String, Object>(); // new model is made to store information
+            return new ModelAndView(model, "index.hbs"); // assemble individual pieces and render
+        }, new HandlebarsTemplateEngine()); //
     }
 }
--- src/test/java/models/HeroTest.java
+++ src/test/java/models/HeroTest.java
@@ -2,6 +2,7 @@ package models;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,5 +14,10 @@ class HeroTest {
 
     @AfterEach
     void tearDown() {
+
+    }
+
+    @Test
+    void name() {
     }
 }
